a87c829980354c25051bf3e609c7673d0aa1ea4b
thedeehapp <deehapp.official@gmail.com>
2026-02-20 15:53:21 +0530

    Create auth.js

---
+++ auth.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+const { register } = require('../controllers/authController');
+
+// @route POST /api/auth/register
+router.post('/register', register);
+
+module.exports = router;
+
