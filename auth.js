14604ded97369b62c73aecfced7d5f3f8158e0cc
thedeehapp <deehapp.official@gmail.com>
2026-02-20 16:18:13 +0530

    Add profile route to fetch user and posts

--- auth.js
+++ auth.js
@@ -24,6 +24,26 @@ module.exports = function(req, res, next) {
         res.status(401).json({ msg: "Token nakli hai!" });
     }
 };
+// @route GET /api/auth/profile/:username
+router.get('/profile/:username', async (req, res) => {
+    try {
+        // User ko dhundo uske username se
+        const user = await User.findOne({ username: req.params.username }).select('-password');
+        if (!user) return res.status(404).json({ msg: "Bhai, ye banda Deeh par nahi mila!" });
+
+        // Is user ki saari posts nikaalo
+        const Post = require('../models/Post');
+        const posts = await Post.find({ user: user._id }).sort({ createdAt: -1 });
+
+        res.json({
+            user,
+            posts,
+            postCount: posts.length
+        });
+    } catch (err) {
+        res.status(500).send("Profile load karne mein mitti ud gayi!");
+    }
+});
 
 
 
